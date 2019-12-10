ed75dcc00fe62a7a0caf5c6969c43fd4191ab67e
awab228 <awab228@yahoo.com>
2019-12-10 02:10:58 +0200

    bootable removed all samsung protections

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 9 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm64"
+#define UTS_VERSION "#9 SMP PREEMPT Tue Dec 10 02:01:20 CAT 2019"
+#define LINUX_COMPILE_BY "awab228"
+#define LINUX_COMPILE_HOST "archPC"
+#define LINUX_COMPILER "gcc version 4.9.x 20150123 (prerelease) (GCC) "
